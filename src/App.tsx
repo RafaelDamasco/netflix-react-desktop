32eb2144b91363ed9fa87f673001424168517245
RafaelDamasco <rafaeldamasco_@hotmail.com>
2022-06-03 18:41:45 -0300

    Estilização dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import Login from './screens/login/login.screen';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import { ThemeProvider } from 'styled-components';
 import theme from './themes/main/theme'
 import { GlobalStyles } from './themes/main/global-styles'
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import MoviesList from './screens/movies-list/movies-list';
+import Login from './screens/login/login.screen';
+
 import { LOGIN_URL } from './screens/login/login.type';
 import { MOVIES_LIST_URL } from './screens/movies-list/movies-list.type';
+
 
 function App() {
   return (
